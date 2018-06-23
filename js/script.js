c12fce31ea90c22c8c0c38bb829c8b74d8370257
OleksandrPavlyshch <lokdawn80@gmail.com>
2018-06-23 22:08:24 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,36 @@
+---
+---
+//Header init
+(($) => {
+	let initHeader = () => {
+		$(window).scroll(function() {
+			let _top = parseInt($(window).height() / 2)
+				, _scroll = parseInt($(window).height() / 3);
+
+			if ($(window).scrollTop() >= _scroll ) {
+				$('#header').addClass('is-scroll');
+			} else {
+				$('#header').removeClass('is-scroll');
+			}
+
+			if ($(window).scrollTop() >= _top) {
+				$('#header').addClass('is-fixed');
+			} else {
+				$('#header').removeClass('is-fixed');
+			}
+		});
+	};
+
+	initHeader();
+
+	//init menu open
+	let $body = $('body')
+		, $menuButton = $('.toggle-menu-button')
+		, menuShowClass = 'is-menu-show';
+
+	$menuButton.on('click', function() {
+		$body.toggleClass(menuShowClass);
+	});
+
+
+})(jQuery);
