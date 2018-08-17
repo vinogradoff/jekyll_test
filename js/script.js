90a6f9102ed8885ad807ce6c0579958e80ac856f
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2018-08-17 21:11:31 +0000

    add testimonials

--- js/script.js
+++ js/script.js
@@ -46,4 +46,14 @@
 		duration: 800
 	});
 
+/* slick slider */
+	$(document).ready(function(){
+		$('.testimonials-slider').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3000
+		});
+	});
+
 })(jQuery);
