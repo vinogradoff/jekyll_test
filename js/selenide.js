741245f8b4ee4123895d0ce958171293e4bbb094
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-07-01 09:11:00 +0300

    truncate multiple trailing slashes from url

--- js/selenide.js
+++ js/selenide.js
@@ -1,18 +1,34 @@
-function rememberLanguageOrRedirect() {
-  var regex = /\/(en|ru)\/.*/;
-  if (window.location.pathname.match(regex)) {
-    var language = window.location.pathname.replace(regex, "$1") || 'en';
-    localStorage.setItem('language', language);
-  }
-  else {
-    var language = localStorage.getItem('language') || 'en';
-    if (language !== 'en' && language !== 'ru') {
-      language = 'en';
-      localStorage.removeItem('language');
+(function () {
+  const path = window.location.pathname
+
+  function truncateTrailingSlashes() {
+    const trailingSlashes = /\/{2,}$/
+    const pathWithoutSlashes = path.match(trailingSlashes) && path.replace(trailingSlashes, '/')
+    if (pathWithoutSlashes) {
+      window.location = pathWithoutSlashes
     }
-
-    window.location = '/' + language + window.location.pathname;
+    return pathWithoutSlashes
   }
-}
 
-rememberLanguageOrRedirect();
+  function rememberLanguage() {
+    const regex = /^\/(en|ru)\//
+    const languageFromPath = path.match(regex) && path.replace(regex, "$1")
+    if (languageFromPath) {
+      localStorage.setItem('language', languageFromPath)
+    }
+    return languageFromPath
+  }
+
+  function redirectToLanguage() {
+    const language = localStorage.getItem('language') || 'en'
+    if (language === 'en' || language === 'ru') {
+      window.location = '/' + language + path
+    }
+    else {
+      localStorage.removeItem('language')
+      window.location = '/' + 'en' + path
+    }
+  }
+
+  truncateTrailingSlashes() || rememberLanguage() || redirectToLanguage()
+}())
